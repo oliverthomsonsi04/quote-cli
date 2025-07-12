3a0d72733beb95d37a5f5c76da311db3e93f8837
Oliver <158419527+oliverthomsonsi04@users.noreply.github.com>
2025-07-12 06:40:01 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const quotes = [
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+  "Strive not to be a success, but rather to be of value. - Albert Einstein",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+];
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  console.log(randomQuote);
+}
+
+showRandomQuote();
