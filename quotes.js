b4881a6c4874e409a8ba7d665720882c5e9778f7
Oliver <158419527+oliverthomsonsi04@users.noreply.github.com>
2025-07-12 06:40:23 +0300

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,8 @@
+const quotes = [
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+  "Strive not to be a success, but rather to be of value. - Albert Einstein",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+];
+
+module.exports = quotes;
